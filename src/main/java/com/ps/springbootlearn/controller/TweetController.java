8a5bed26edf3fe0f124159d3b94fb83504a0c91a
paulstarbi <Pawel_Stabryla@epam.com>
2017-09-14 13:07:11 +0200

    Poruszanie się po witrynie

--- src/main/java/com/ps/springbootlearn/controller/TweetController.java
+++ src/main/java/com/ps/springbootlearn/controller/TweetController.java
@@ -7,8 +7,11 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
@@ -19,11 +22,27 @@ public class TweetController {
     private Twitter twitter;
 
     @RequestMapping("/")
-    public String hello(@RequestParam(defaultValue = "SpringMVC") String search,
-                        Model model){
+    public String home() {
+        return "searchPage";
+    }
 
-        SearchResults searchResult = twitter.searchOperations().search(search);
-        List<Tweet> tweets = searchResult.getTweets();
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request,
+                             RedirectAttributes redirectAttributes) {
+        String search = request.getParameter("search");
+        if(search.toLowerCase().contains("smieci")) {
+            redirectAttributes.addAttribute("error", "Sprobuj wpisac Spring");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
+    }
+
+    @RequestMapping("/result")
+    public String hello(@RequestParam(defaultValue = "masterSpring") String search,
+                        Model model) {
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        List<Tweet> tweets = searchResults.getTweets();
 
         model.addAttribute("tweets", tweets);
         model.addAttribute("search", search);
